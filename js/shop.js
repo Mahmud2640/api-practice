62a83bed07db7af89e49d2d53db6ed44d0f9752b
Mahmud2640 <mdmahmud484@gmail.com>
2022-02-26 15:57:23 +0600

    added

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,4 @@
-const shop = {
+/* const shop = {
   name: "Taher Store",
   address: "Rustom Ali Collage Road",
   profit: 20000,
@@ -10,4 +10,35 @@ const shop = {
 const shopStringFild = JSON.stringify(shop);
 console.log(shopStringFild);
 const convarted = JSON.parse(shopStringFild);
-console.log(convarted);
+console.log(convarted); */
+
+/* New part Start */
+
+function lodeUsers() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((users) => displayUsers(users));
+}
+
+function lodePosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+function lodeComments() {
+  fetch("https://jsonplaceholder.typicode.com/comments")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+/* Display Show */
+
+function displayUsers(users) {
+  const ul = document.getElementById("users");
+  for (const user of users) {
+    console.log(user.name);
+    const li = document.createElement("li");
+    li.innerText = user.name;
+    ul.appendChild(li);
+  }
+}
